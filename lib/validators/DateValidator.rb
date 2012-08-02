40d88c6437eb2f2785ec662b3328cd8561e17433
Prtaheek Totiger <prathik.t90@gmail.com>
2012-08-02 13:01:59 -0400

    deleted .rvmrc

--- lib/validators/DateValidator.rb
+++
@@ -1,9 +0,0 @@
-class DateValidator < ActiveModel::EachValidator
-
-def validate_each(record, attribute, value)
-	
-	if !value.blank? and value < record[options[:begin]]
-	record.errors[attribute] << (options[:message] || "should be dated before Start date")
-	end
-end
-end
