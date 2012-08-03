80e3f41597721c6972826a223d8deef5dae9d3e2
Dave Newton <davelnewton@gmail.com>
2012-08-03 09:56:44 -0400

    Fixed view spec.

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'sqlite3'
 group :test do
   gem "rspec-rails"
   gem "rspec-mocks"   
+  gem "capybara"
 end
 
 # Gems used only for assets and not required
--- spec/views/ads/index.html.haml_spec.rb
+++ spec/views/ads/index.html.haml_spec.rb
@@ -2,10 +2,9 @@ require "spec_helper"
 
 describe "ads/index.html.haml" do
 
-	it "should have the h1 'Cusotm Validators'" do
-	visit '/ads'
-	page.should have_selector('h1', :text => "Custom Validators")
-end
-	
-end
+  it "should have the h1 'Custom Validators'" do
+    render
+    rendered.should have_selector('h1', :text => 'Custom Validators')
+  end
 
+end
